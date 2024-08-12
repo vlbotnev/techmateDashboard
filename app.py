f990ccb28e4804f9f3dbf8ce7c5ec15b3225c415
vlbotnev <vlbotnev@mail.ru>
2024-08-12 21:37:26 +0300

    Dashboard update v1.1_2024-08-12

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template, Response
+import requests
 
 app = Flask(__name__)
 
@@ -11,6 +11,19 @@ def index():
         filename = f.filename
         return render_template('index.html', filename=filename)
     return render_template('index.html', filename=None)
+
+
+@app.route('/start-analysis', methods=['POST'])
+def start_analysis():
+    f = request.files['file']
+    if f:
+        # URL удаленного API, куда будет отправлен файл
+        api_url = 'http://10.88.88.90:3000/upload/'
+        # Отправляем файл на удаленное API
+        response = requests.post(api_url, files={'file': (f.filename, f.stream, f.content_type)})
+        # Возвращаем ответ API, как есть
+        return Response(response.content, response.status_code, response.headers.items())
+    return "no file provided"
 
 
 if __name__ == "__main__":
